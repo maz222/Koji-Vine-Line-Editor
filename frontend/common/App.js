7674ea27c403247b7273cb4d28d9ea17a3254b71
maz2 <maz2@koji-projects.com>
2020-02-21 10:03:45 +0000

    Manual deploy

--- frontend/common/App.js
+++ frontend/common/App.js
@@ -2,257 +2,181 @@ import React from 'react';
 import styled, { keyframes } from 'styled-components';
 import Koji from '@withkoji/vcc';
 import CustomVCC from '@withkoji/custom-vcc-sdk';
-import { StyledToolBar,
-    StyledToolButton,
-    StyledButtonInner,
-    StyledRow,
-    StyledCellImage,
-    StyledLevel,
-    StyledError } from './App.styled';
 
-const Container = styled.div`
-    background-color: ${() => Koji.config.colors.backgroundColor};
-    padding: 20px
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    font-size: calc(10px + 2vmin);
-    color: ${() => Koji.config.colors.textColor};
-    text-align: center;
-    overflow: hidden;
+import {GridBar,CellBar} from './TopBar.js';
+
+const DEFAULT_ROWS = 10;
+const DEFAULT_COLS = 10;
+
+let GridCell = styled.div`
+    background-color:rgb(100,100,100);
+    width:40px;
+    height:40px;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    &:hover {
+        background-color:rgb(60,60,60);
+    }
 `;
 
+let CellImage = styled.img`
+    width:20px;
+    height:20px;
+`;
+
+const IMAGES = [
+    null,
+    Koji.config.images.spawn,
+    Koji.config.images.coin,
+    Koji.config.images.wall,
+    Koji.config.images.end
+];
+
 class App extends React.Component {
-  EMPTY = 0;
-  PLAYER = 1;
-  BOX = 2;
-  WALL = 3;
-  END = 4;
-  images = [];
-  groundImage = '';
-  endImage = '';
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-        'activeTool': 0,
-        'value': {
-          level:[[0]],
-          ends: []
-        },
-        'errors': []
-    };
-
-    this.images[this.EMPTY] = '';
-    this.images[this.PLAYER] = Koji.config.images.player;
-    this.images[this.BOX] = Koji.config.images.box;
-    this.images[this.WALL] = Koji.config.images.wall;
-    this.groundImage = Koji.config.images.ground;
-    this.endImage = Koji.config.images.end;
-    this.customVCC = new CustomVCC();
-  }
-
-  componentDidMount() {
-    this.customVCC.register('300', '300');
-    this.customVCC.onUpdate(({ value }) => {
-      if(value.level == undefined) {
-        value = {
-          level:[[0]],
-          ends: []
+    constructor(props) {
+        super(props);
+        this.customVCC = new CustomVCC();
+        let level = [];
+        for(var i=0;i<DEFAULT_ROWS;i++) {
+            level.push([]);
+            for(var j=0;j<DEFAULT_COLS;j++) {
+                level[i].push(0);                
+            }
+        }
+        this.state = {
+            level:level,
+            activeItem:0,
+            startCell:null,
+            exitCell:null,
         };
-      }
-      this.setState({ value: value });
-    });
-  };
+    }
 
-  change = (value) => {
-    this.customVCC.change(value);
-    this.customVCC.save();
-  };
+    componentDidMount() {
+        this.customVCC.register('300','300');
+    }
 
-  setTool = (tool) => {
-    this.setState({'activeTool': tool});
-  };
-
-  getErrors = (value) => {
-      const { level, ends } = value;
-      let errors = [];
-      let boxCount = 0;
-      let hasPlayer = false;
-      for (var i = level.length - 1; i >= 0; i--) {
-        for (var j = level[i].length - 1; j >= 0; j--) {
-            if (level[i][j] == this.BOX) {
-                boxCount++;
-            }
-            if (level[i][j] == this.WALL && this.isEnd(j, i)) {
-                errors.push('Wall intersects exit');
-            }
-            if (level[i][j] == this.PLAYER) {
-                hasPlayer = true;
-            }
+    expandX() {
+        let level = this.state.level;
+        for(var i=0; i<level.length; i++) {
+            level[i].push(0);
         }
-      }
-      if (boxCount != ends.length) {
-          errors.push('Number of boxes does not equal number of exits');
-      }
-      if (!hasPlayer) {
-          errors.push('No player in level');
-      }
-      for (var i = ends.length - 1; i >= 0; i--) {
-          if (ends[i].y >= level.length || ends[i].x >= level[0].length) {
-              errors.push('Exit outside of level (' + (ends[i].x+1) + ', ' + (ends[i].y+1) + ')')
-          }
-      }
-      return errors;
-  };
+        this.state = {level:level};
+        this.forceUpdate();
+        this.customVCC.change(this.state.level);
+        this.customVCC.save();
+    }
 
-  clearPlayer = (level) => {
-    out:
-    for (var i = level.length - 1; i >= 0; i--) {
-      for (var j = level[i].length - 1; j >= 0; j--) {
-        if (level[i][j] == this.PLAYER) {
-          level[i][j] = this.EMPTY;
-          break out;
+    shrinkX() {
+        let level = this.state.level;
+        //min level size of 1x1
+        if(level[0].length == 1) {
+            return;
         }
-      }
-    }
-    return level;
-  };
-
-  isEnd = (x,y) => {
-    const ends = this.state.value.ends;
-    for (var i = ends.length - 1; i >= 0; i--) {
-      if (ends[i].x == x && ends[i].y == y) {
-        return true;
-      }
-    }
-    return false;
-  };
-
-  removeEnd = (x,y, ends) => {
-    let toRemove = null;
-    for (var i = ends.length - 1; i >= 0; i--) {
-      if (ends[i].x == x && ends[i].y == y) {
-        toRemove = i;
-      }
-    }
-    if (toRemove !== null) {
-      ends.splice(toRemove,1);
-    }
-    return ends;
-  };
-
-  tileClick = (x,y) => {
-    const { activeTool } = this.state;
-    let { value } = this.state;
-
-    if (activeTool != this.END) {
-      if (activeTool == this.PLAYER) {
-        value.level = this.clearPlayer(value.level);
-      }
-      if (!(activeTool == this.WALL && this.isEnd(x,y))) {
-        value.level[y][x] = activeTool;
-      }
-    } else {
-      if (this.isEnd(x,y)) {
-        value.ends = this.removeEnd(x,y, value.ends);
-      } else {
-        value.ends.push({x:x,y:y});
-      }
+        for(var i=0;i<level.length;i++) {
+            level[i].pop();
+        }
+        this.state = {level:level};
+        this.forceUpdate();
+        this.customVCC.change(this.state.level);
+        this.customVCC.save();
     }
 
-    this.setState({ value: value, errors: this.getErrors(value) })
-    this.change(value);
-  };
-
-  addRow = () => {
-    let { value } = this.state;
-    value.level.push(Array.from({ length: value.level[0].length }));
-    this.setState({ value: value, errors: this.getErrors(value) })
-    this.change(value);
-  };
-
-  removeRow = () => {
-    let { value } = this.state;
-    value.level.splice( value.level.length-1, 1);
-    this.setState({ value: value, errors: this.getErrors(value) })
-    this.change(value);
-  };
-
-  addColumn = () => {
-    let { value } = this.state;
-    for (var i = value.level.length - 1; i >= 0; i--) {
-      value.level[i].push(this.EMPTY);
+    expandY() {
+        let level = this.state.level;
+        let t = [];
+        for(var i=0;i<level[0].length;i++) {
+            t.push(0);
+        }
+        level.push(t);
+        this.state = {level:level};
+        this.forceUpdate();
+        this.customVCC.change(this.state.level);
+        this.customVCC.save();
     }
-    this.setState({ value: value })
-    this.change(value);
-  };
-  removeColumn = () => {
-    let { value } = this.state;
-    for (var i = value.level.length - 1; i >= 0; i--) {
-      value.level[i].splice(value.level[i].length-1,1);
-    }
-    this.setState({ value: value })
-    this.change(value);
-  };
 
-  render() {
-    const { activeTool, errors } = this.state;
-    const level = this.state.value.level; 
-    return (
-      <Container>
-        <StyledToolBar key={'tiles'}>
-          <StyledToolButton key={0} onClick={()=>{this.setTool(0)}} className={activeTool == this.EMPTY ? 'active': ''}>
-            <StyledButtonInner backgroundImage={this.groundImage} />
-          </StyledToolButton>
-          <StyledToolButton key={1} onClick={()=>{this.setTool(1)}} className={activeTool == this.PLAYER ? 'active': ''}>
-            <StyledButtonInner backgroundImage={this.images[this.PLAYER]} />
-          </StyledToolButton>
-          <StyledToolButton key={2} onClick={()=>{this.setTool(2)}} className={activeTool == this.BOX ? 'active': ''}>
-            <StyledButtonInner backgroundImage={this.images[this.BOX]} />
-          </StyledToolButton>
-          <StyledToolButton key={3} onClick={()=>{this.setTool(3)}} className={activeTool == this.WALL ? 'active': ''}>
-            <StyledButtonInner backgroundImage={this.images[this.WALL]} />
-          </StyledToolButton>
-          <StyledToolButton key={4} onClick={()=>{this.setTool(4)}} className={activeTool == this.END ? 'active': ''}>
-            <StyledButtonInner backgroundImage={this.endImage} />
-          </StyledToolButton>
-        </StyledToolBar>
-        <StyledToolBar key={'grid'}>
-          <StyledToolButton key={0} onClick={this.addColumn}>|+</StyledToolButton>
-          <StyledToolButton key={1} onClick={this.removeColumn}>|-</StyledToolButton>
-          <StyledToolButton key={2} onClick={this.addRow}>&mdash; +</StyledToolButton>
-          <StyledToolButton key={3} onClick={this.removeRow}>&mdash; -</StyledToolButton>
-        </StyledToolBar>
-        <StyledLevel>
-          {level.map((row, row_index) => {
-            return(
-              <StyledRow key={row_index}>
-                {row.map((cell, cell_index) => {
-                  return(
-                    <StyledCellImage
-                      key={cell_index}
-                      occupant={this.images[cell]}
-                      ground={this.groundImage}
-                      end={this.isEnd(cell_index, row_index) ? this.endImage : '' }
-                      onClick={ ()=>{this.tileClick(cell_index,row_index);} }
-                      />
-                  )
-                })}
-              </StyledRow>
-            )
-          })}
-        </StyledLevel>
-        {errors.map((error, error_id) => {
-            return(
-                <StyledError key={error_id}>{error}</StyledError>
-            )
-        })}
-      </Container>
-    );
-  }
+    shrinkY() {
+        let level = this.state.level;
+        //min level size of 1x1
+        if(level.length == 1) {
+            return;
+        }
+        level.pop();
+        this.state = {level:level};
+        this.forceUpdate();
+        this.customVCC.change(this.state.level);
+        this.customVCC.save();
+    }
+
+    resetCell(row,col) {
+        //reset spawn
+        if(this.state.level[row][col] == 1) {
+            this.setState({startCell:null});
+        }
+        //reset goal
+        if(this.state.level[row][col] == 4) {
+            this.setState({exitCell:null});
+        }
+        this.state.level[row][col] = 0;
+    }
+
+    setCell(row,col,value) {
+        this.resetCell(row,col);
+        //set spawn
+        if(value == 1) {
+            if(this.state.startCell != null) {
+                this.resetCell(this.state.startCell[0],this.state.startCell[1]);
+            }
+            this.setState({startCell:[row,col]});
+        }
+        //set goal
+        if(value == 4) {
+            if(this.state.exitCell != null) {
+                this.resetCell(this.state.exitCell[0],this.state.exitCell[1]);
+            }
+            this.setState({exitCell:[row,col]});
+        }
+        this.state.level[row][col] = value;
+        this.forceUpdate();
+        this.customVCC.change(this.state.level);
+        this.customVCC.save();
+    }
+
+    render() {
+        let PageDiv = styled.div`
+            padding:50px;
+            width:calc(100% - 100px);
+            height:calc(100% - 100px);
+            display:flex;
+            align-items:center;
+            justify-content:center;
+            flex-direction:column;
+            background-color:rgb(240,240,240);
+        `;
+        return (
+            <PageDiv>
+                <GridBar expandY={() => {this.expandY()}} expandX={() => {this.expandX()}} shrinkX={() => {this.shrinkX()}} shrinkY={() => {this.shrinkY()}}/>
+                <CellBar activeButton={this.state.activeItem} update={(i) => {this.setState({activeItem:i})}}/>
+                <h3>{"Level must have a spawn and a goal!"}</h3>
+                <div style={{display:'flex',flexDirection:'column', margin:'10px 0 0 0'}}>
+                {
+                    this.state.level.map((row,rowIndex) => {
+                        return(
+                            <div style={{display:'flex'}}>
+                                {
+                                    row.map((cell,cellIndex) => {
+                                        let Image = <CellImage src={IMAGES[cell]} />;
+                                        return(<GridCell onClick={() => {this.setCell(rowIndex,cellIndex,this.state.activeItem);}}>{Image}</GridCell>);
+                                    })
+                                }
+                            </div>
+                        );
+                    })
+                }
+                </div>
+            </PageDiv>
+        );
+    }
 }
 
 export default App;
---
+++ frontend/common/TopBar.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+import Koji from '@withkoji/vcc';
+
+
+var Bar = styled.div`
+	background-color:rgb(60,60,60);
+	border:1px solid rgba(0,0,0,.2);
+	border-radius:4px;
+	display:flex;
+	justify-content:space-around;
+	align-items:center;
+	padding:20px;
+	width:calc(100% - 40px);
+    margin-bottom:10px;
+`;
+
+let ButtonImage = styled.img`
+    width:50px;
+    height:50px;
+`;
+
+var Button = styled.button`
+	background-color:rgb(100,100,100);
+	border:1px solid rgba(0,0,0,.1);
+	border-radius:4px;
+	padding:5px;
+    margin:5px;
+	&:hover {
+		background-color:rgb(80,80,80);
+	}
+`;
+
+var ActiveButton = styled.button`
+	background-color:rgb(200,200,200);
+	border:3px solid black;
+	border-radius:4px;
+	padding:10px;
+`;
+
+const IMAGES = [
+    null,
+    Koji.config.images.spawn,
+    Koji.config.images.coin,
+    Koji.config.images.wall,
+    Koji.config.images.end
+]
+
+class GridBar extends React.Component {
+	render() {
+        let VCC = Koji.config.images;
+		return(
+			<Bar>
+				<Button onClick={this.props.expandX}><ButtonImage src={VCC.growX}/></Button>
+				<Button onClick={this.props.shrinkX}><ButtonImage src={VCC.shrinkX}/></Button>
+				<Button onClick={this.props.expandY}><ButtonImage src={VCC.growY}/></Button>
+				<Button onClick={this.props.shrinkY}><ButtonImage src={VCC.shrinkY}/></Button>
+			</Bar>
+		);
+	}
+}
+
+class CellBar extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		let buttons = [0,1,2,3,4];
+        let labels = ["Floor","Spawn","Coin","Wall","Goal"];
+		return(
+			<Bar>
+			{
+				buttons.map((image,index) => {
+					let Image = <ButtonImage src={IMAGES[image]} />
+					let ItemButton = index == this.props.activeButton ? <ActiveButton onClick={() => {this.props.update(index);}} >{Image}</ActiveButton> : 
+						<Button onClick={() => {this.props.update(index);}}>{Image}</Button>
+                    let labelColor = index == this.props.activeButton ? 'red' : 'white';
+                    return(
+                        <div style={{display:'flex',flexDirection:'column',alignItems:'center'}}>
+                            {ItemButton}
+                            <h2 style={{fontSize:'1em',color:labelColor,margin:'5px 0 0 0', backgroundColor:'rgb(40,40,40)',padding:'4px 10px 4px 10px',borderRadius:'2px'}}>{labels[image]}</h2>
+                        </div>
+                    );
+				})
+			}
+			</Bar>
+		)
+	}
+}
+
+export {GridBar,CellBar};
