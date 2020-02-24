c8605c0c4c8184bbfa38f7a48cd98b770dd3a661
maz2 <maz2@koji-projects.com>
2020-02-24 05:10:19 +0000

    Manual deploy

--- frontend/common/App.js
+++ frontend/common/App.js
@@ -59,9 +59,9 @@ class App extends React.Component {
     componentDidMount() {
         this.customVCC.register('300','300');
         this.customVCC.onUpdate((newProps) => {
-            //if(newProps.value != "" && newProps.value != undefined) {
-            //    this.setState({level:newProps.value});
-            //}
+            if(newProps.value != "" && newProps.value != undefined) {
+                this.setState({level:newProps.value});
+            }
         })
     }
 
